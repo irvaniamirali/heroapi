2d0eb4ac55497fa128a54e97b202c9a08953dcfd
amirali irvany <dev.amirali.irvany@gmail.com>
2024-02-17 14:36:40 +0330

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 @app.get('/', status_code=status.HTTP_200_OK)
 async def main() -> dict:
-    '''This function is for displaying developer information'''
+    '''displaying developer information'''
     return {
         'status': True,
         'dev': 'amirali irvany',
@@ -19,7 +19,17 @@ async def main() -> dict:
 parameters: list = [{'item': 'url', 'item': 'timeout'}]
 @app.get('/rubino', status_code=status.HTTP_200_OK)
 async def rubino_dl(url: str, timeout: float = 10) -> dict:
-    '''Powered by the myrino library. github > github.com/metect/myrino'''
+    '''This method is used to get the download link
+    and other information of the post(s) in Rubino Messenger
+    :param url:
+        The link of the desired post
+    :param timeout:
+        Optional To manage slow timeout when the server is slow
+    :return:
+        Full post information
+
+    Powered by the myrino library. github > github.com/metect/myrino
+    '''
     result: dict = rubino(url=url, timeout=timeout)
     return {
         'status': True,
@@ -32,6 +42,10 @@ async def rubino_dl(url: str, timeout: float = 10) -> dict:
 parameters: list = [{'item': 'text'}]
 @app.get('/font', status_code=status.HTTP_200_OK)
 async def main(text: str) -> dict:
+    '''This function is for generating fonts. Currently only English language is supported
+    :param text:
+        The text you want the font to be applied to
+    '''
     result: list = font(text=text)
     return {
         'status': True,
@@ -44,9 +58,33 @@ async def main(text: str) -> dict:
 parameters: list = [{'item': 'text'}]
 @app.get('/lang', status_code=status.HTTP_200_OK)
 async def lang_detect(text: str) -> dict:
-    '''Powered by the `langdetetc` library'''
+    '''This function is to identify the language of a text
+    :param text:
+        Your desired text
+    :return:
+        example: `en` or `fa`
+
+    Powered by the `langdetetc` library
+    '''
     result: str = lang(text=text)
-    print(result)
+    return {
+        'status': True,
+        'dev': 'amirali irvany',
+        'url': 't.me/ohmyapi',
+        'result': result
+    }
+
+
+parameters: list = [{'item': 'content', 'item': 'count', 'item': 'lang'}]
+@app.get('/fake', status_code=status.HTTP_200_OK)
+async def main(content: str = 'text', count: int = 10, lang: str = 'en_US') -> dict:
+    MAX_COUNT_DATA: int = 999
+    if count > MAX_COUNT_DATA:
+        return {
+            'err_message': f'It is not possible to provide more than {MAX_COUNT_DATA} data'
+        }
+
+    result = faker_data(content=content, count=count, lang=lang)
     return {
         'status': True,
         'dev': 'amirali irvany',
