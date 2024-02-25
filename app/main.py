24e4d77c73ae9fd77fdef4c7a4951ccb4f6cec96
amirali irvany <dev.amirali.irvany@gmail.com>
2024-02-25 22:50:46 +0330

    Update app/main.py

--- app/main.py
+++ app/main.py
@@ -94,9 +94,9 @@ async def fake_text(count: int = 100, lang: str = 'en_US') -> dict:
 
 @app.get('/api/datetime', status_code=status.HTTP_200_OK)
 @app.post('/api/datetime', status_code=status.HTTP_200_OK)
-async def datetime() -> dict:
+async def datetime(tr_num: str = 'en') -> dict:
     '''This api is used to display date and time in solar'''
-    return await heroapi._datetime()
+    return await heroapi._datetime(tr_num=tr_num)
 
 
 @app.get('/api/usd', status_code=status.HTTP_200_OK)
@@ -104,3 +104,18 @@ async def datetime() -> dict:
 async def _usd() -> dict:
     '''api to get live currency prices from the `https://irarz.com` website'''
     return await heroapi.usd()
+
+
+parameters: list = [{'item': 'text'}]
+@app.get('/api/bard', status_code=status.HTTP_200_OK)
+@app.post('/api/bard', status_code=status.HTTP_200_OK)
+async def bard(text : str) -> dict:
+    '''bard artificial intelligence api'''
+    return await heroapi._bard(text=text)
+
+
+@app.get('/api/joke', status_code=status.HTTP_200_OK)
+@app.post('/api/joke', status_code=status.HTTP_200_OK)
+async def joke() -> dict:
+    '''Happy jokes web service. It has about 80 different jokes'''
+    return await heroapi._joke()
