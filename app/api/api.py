ee3d680ead05f6fbe4f23fb637078b0a37b54e00
amirali irvany <dev.amirali.irvany@gmail@gmail.com>
2024-03-14 19:55:20 +0330

    Added `pypi_search` method to app/api/api.py

--- app/api/api.py
+++ app/api/api.py
@@ -263,3 +263,30 @@ class HeroAPI:
             return await self.execute(success=False, data='A problem has occurred on our end')
 
         return await self.execute(success=True, data=responce.json())
+
+
+    async def pypi_search(self):
+        query = '+'.join(query.split())
+        request = requests.request(method='GET', url=f'https://pypi.org/search/?q={query}')
+        if request.status_code != requests.codes.ok:
+            return await self.execute(success=False, data='A problem has occurred on our end')
+
+        soup = bs4.BeautifulSoup(response.text, 'html.parser')
+        package_snippets = soup.find_all('a', class_='package-snippet')
+        search_results = list()
+        for package_snippet in package_snippets:
+            span_elems = package_snippet.find_all('span')
+            name = span_elems[0].text.strip()
+            version = span_elems[1].text.strip()
+            release_date = span_elems[2].text.strip()
+            description = package_snippet.p.text.strip()
+            search_results.append(
+                dict(
+                    name=name,
+                    version=version,
+                    release_date=release_date,
+                    description=description
+                )
+            )
+
+        return await self.execute(success=True, data=search_results)
