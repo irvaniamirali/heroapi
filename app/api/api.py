0bb77f5da3437a71bb20e36dbfd69fd2a51cdba7
amirali irvany <dev.amirali.irvany@gmail@gmail.com>
2024-03-14 19:25:34 +0330

    Added `font` method to app/api/api.py

--- app/api/api.py
+++ app/api/api.py
@@ -68,3 +68,27 @@ class HeroAPI:
         ascii_image = [new_pixels[index:index + 120]
         for index in range(0, new_pixels_count, 120)]
         return await self.execute(success=True, data='\n'.join(ascii_image))
+
+
+    async def font(self, text: str):
+        if langdetect.detect(text) in ['fa', 'ar', 'ur']:
+            return await self.execute(
+                success=False, err_message='Currently, Persian language is not supported'
+            )
+        else:
+            with open('app/jsonfiles/font.json', 'r') as f:
+                fonts = json.load(f)
+
+            converted_text = str()
+            for count in range(0, len(fonts)):
+                for char in text:
+                    if char.isalpha():
+                        char_index = ord(char.lower()) - 97
+                        converted_text += fonts[str(count)][char_index]
+                    else:
+                        converted_text += char
+
+                converted_text += '\n'
+                final_values = converted_text.split('\n')[0:-1]
+
+        return await self.execute(success=True, data=final_values)
