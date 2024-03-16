c240a69175ac432c8a938e4a256d4ff54b5df907
amirali irvany <dev.amirali.irvany@gmail@gmail.com>
2024-03-16 21:28:18 +0330

    Added `pypi package search` web service to app/api/api.py

--- app/main.py
+++ app/main.py
@@ -333,7 +333,31 @@ async def github_topic_search(request: Request, query: str, per_page: int = 30, 
 @limiter.limit(limit_value=LIMITER_TIME, key_func=get_remote_address)
 async def pypi_search(request: Request, query: str) -> dict:
     '''PyPi package search web service'''
-    return await api.pypi_search(query=query)
+    query = '+'.join(query.split())
+    request = requests.request(method='GET', url=f'https://pypi.org/search/?q={query}')
+    if request.status_code != requests.codes.ok:
+        return await execute(success=False, data='A problem has occurred on our end')
+
+    soup = bs4.BeautifulSoup(request.text, 'html.parser')
+    package_snippets = soup.find_all('a', class_='package-snippet')
+
+    search_results = list()
+    for package_snippet in package_snippets:
+        span_elems = package_snippet.find_all('span')
+        name = span_elems[0].text.strip()
+        version = span_elems[1].text.strip()
+        release_date = span_elems[2].text.strip()
+        description = package_snippet.p.text.strip()
+        search_results.append(
+            dict(
+                name=name,
+                version=version,
+                release_date=release_date,
+                description=description
+            )
+        )
+
+    return await execute(success=True, data=search_results)
 
 
 @app.get('/api/divar', tags=['Other'], status_code=status.HTTP_200_OK)
