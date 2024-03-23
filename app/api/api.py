79b46140502dda89a223320b10df8bbcb5fc6c8c
amirali irvany <dev.amirali.irvany@gmail.com>
2024-03-23 19:46:54 +0330

    Update app/api/api.py

--- app/api/api.py
+++ app/api/api.py
@@ -46,4 +46,25 @@ class ohmyapi:
         return await self.execute(success=True, data=final_responce)
 
 
+    async def font(self, text: str) -> dict:
+        if langdetect.detect(text) in ['fa', 'ar', 'ur']:
+            return await self.execute(
+                success=False, err_message='Currently, Persian language is not supported'
+            )
+        else:
+            with open('app/jsonfiles/font.json', 'r') as f:
+                fonts = json.load(f)
 
+            converted_text = str()
+            for count in range(0, len(fonts)):
+                for char in text:
+                    if char.isalpha():
+                        char_index = ord(char.lower()) - 97
+                        converted_text += fonts[str(count)][char_index]
+                    else:
+                        converted_text += char
+
+                converted_text += '\n'
+                final_values = converted_text.split('\n')[0:-1]
+
+            return await self.execute(success=True, data=final_values)
