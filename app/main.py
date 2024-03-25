d1a6d377ea3a0db3af803f91bd866317857c3d82
amirali irvany <dev.amirali.irvany@gmail.com>
2024-03-25 22:56:15 +0330

    Update app/main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,11 @@
-from fastapi import FastAPI, Request, File, status
+from fastapi import FastAPI, Request, status
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.openapi.docs import get_swagger_ui_html
 
-from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.errors import RateLimitExceeded
-from slowapi.util import get_remote_address
+from app.router.router import router
 
-from app.api.api import sharkAPI
 
-# Instances
-api = sharkAPI()
 app = FastAPI(
     title='sharkAPI',
     description='Free and open source api',
@@ -31,11 +26,6 @@ app = FastAPI(
 templates = Jinja2Templates(directory='app/templates')
 app.mount('/app/static', StaticFiles(directory='app/static'), name='static')
 
-limiter = Limiter(key_func=get_remote_address)
-app.state.limiter, LIMITER_TIME = limiter, '1000/minute'
-app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
-
-
 @app.get('/docs', include_in_schema=False)
 async def swagger_ui_html():
     return get_swagger_ui_html(
@@ -53,140 +43,5 @@ async def custom_404_handler(request: Request, __):
         }
     )
 
-
-@app.get('/api/bard', tags=['AI'], status_code=status.HTTP_200_OK)
-@app.post('/api/bard', tags=['AI'], status_code=status.HTTP_200_OK)
-@limiter.limit(limit_value=LIMITER_TIME, key_func=get_remote_address)
-async def bard_ai(request: Request, prompt: str) -> dict:
-    '''Bard artificial intelligence web service'''
-    return await api.bard(prompt=prompt)
-
-
-@app.get('/api/font', tags=['Art'], status_code=status.HTTP_200_OK)
-@app.post('/api/font', tags=['Art'], status_code=status.HTTP_200_OK)
-@limiter.limit(limit_value=LIMITER_TIME, key_func=get_remote_address)
-async def font(request: Request, text: str) -> dict:
-    '''Generate ascii fonts. Currently only English language is supported'''
-    return await api.font(text=text)
-
-
-@app.get('/api/datetime', tags=['Data & time'], status_code=status.HTTP_200_OK)
-@app.post('/api/datetime', tags=['Data & time'], status_code=status.HTTP_200_OK)
-@limiter.limit(limit_value=LIMITER_TIME, key_func=get_remote_address)
-async def datetime(request: Request, tr_num: str = 'en') -> dict:
-    '''Display detailed information about the date of the solar calendar'''
-    return await api.datetime(tr_num=tr_num)
-
-
-@app.get('/api/convert-date', tags=['Date & time'], status_code=status.HTTP_200_OK)
-@app.post('/api/convert-date', tags=['Date & time'], status_code=status.HTTP_200_OK)
-@limiter.limit(limit_value=LIMITER_TIME, key_func=get_remote_address)
-async def convert_date(request: Request, day: int, month: int, year: int) -> dict:
-    '''Convert Shamsi date to Gregorian'''
-    return await api.convert_date(day=day, month=month, year=year)
-
-
-@app.get('/api/faker', tags=['Fake data'], status_code=status.HTTP_200_OK)
-@app.post('/api/faker', tags=['Fake data'], status_code=status.HTTP_200_OK)
-@limiter.limit(limit_value=LIMITER_TIME, key_func=get_remote_address)
-async def fake_data(request: Request, item: str, count: int = 100, lang: str = 'en') -> dict:
-    '''Production fake data. items: (`text`, `name`, `email`)'''
-    return await api.fake_data(item=item, count=count, lang=lang)
-
-
-@app.get('/api/lang', tags=['Identify language'], status_code=status.HTTP_200_OK)
-@app.post('/api/lang', tags=['Identify language'], status_code=status.HTTP_200_OK)
-@limiter.limit(limit_value=LIMITER_TIME, key_func=get_remote_address)
-async def language_detect(request: Request, text: str) -> dict:
-    '''Identifying the language of texts'''
-    return await api.language_detect(text=text)
-
-
-@app.get('/api/location', tags=['Location'], status_code=status.HTTP_200_OK)
-@app.post('/api/location', tags=['Location'], status_code=status.HTTP_200_OK)
-@limiter.limit(limit_value=LIMITER_TIME, key_func=get_remote_address)
-async def location(request: Request, text: str, latitude: float, longitude: float) -> dict:
-    '''Web service to get location and map'''
-    return await api.location(text=text, latitude=latitude, longitude=longitude)
-
-
-@app.get('/api/music-fa', tags=['Music search'], status_code=status.HTTP_200_OK)
-@app.post('/api/music-fa', tags=['Music search'], status_code=status.HTTP_200_OK)
-@limiter.limit(limit_value=LIMITER_TIME, key_func=get_remote_address)
-async def music_fa(request: Request, query: str, page: int = 1) -> dict:
-    '''Search and search web service on the [music-fa](https://music-fa.com) site'''
-    return await api.music_fa(query=query, page=page)
-
-
-@app.get('/api/news', tags=['News'], status_code=status.HTTP_200_OK)
-@app.post('/api/news', tags=['News'], status_code=status.HTTP_200_OK)
-@limiter.limit(limit_value=LIMITER_TIME, key_func=get_remote_address)
-async def news(request: Request, page: int = 1) -> dict:
-    '''Web service to display news. onnected to the site www.tasnimnews.com'''
-    return await api.news(page=page)
-
-
-@app.get('/api/rubino', tags=['Social media'], status_code=status.HTTP_200_OK)
-@app.post('/api/rubino', tags=['Social media'], status_code=status.HTTP_200_OK)
-@limiter.limit(limit_value=LIMITER_TIME, key_func=get_remote_address)
-async def rubino(request: Request, auth: str, url: str, timeout: float = 10) -> dict:
-    '''This api is used to get the information of the post(s) in Rubino Messenger'''
-    return await api.rubino(auth=auth, url=url, timeout=timeout)
-
-
-@app.get('/api/translate', tags=['Translate'], status_code=status.HTTP_200_OK)
-@app.post('/api/translate', tags=['Translate'], status_code=status.HTTP_200_OK)
-@limiter.limit(limit_value=LIMITER_TIME, key_func=get_remote_address)
-async def translate(request: Request, text: str, to_lang: str = 'auto', from_lang: str = 'auto') -> dict:
-    '''Translation of texts based on the Google Translate engine'''
-    return await api.translate(text=text, to_lang=to_lang, from_lang=from_lang)
-
-
-@app.get('/api/github-topic-search', tags=['Github'], status_code=status.HTTP_200_OK)
-@app.post('/api/github-topic-search', tags=['Github'], status_code=status.HTTP_200_OK)
-@limiter.limit(limit_value=LIMITER_TIME, key_func=get_remote_address)
-async def github_topic_search(request: Request, query: str, per_page: int = 30, page: int = 1) -> dict:
-    '''Github topic search web service'''
-    return await api.github_topic_search(query=query, per_page=per_page, page=page)
-
-
-@app.get('/api/github-repo-search', tags=['Github'], status_code=status.HTTP_200_OK)
-@app.post('/api/github-repo-search', tags=['Github'], status_code=status.HTTP_200_OK)
-@limiter.limit(limit_value=LIMITER_TIME, key_func=get_remote_address)
-async def github_repo_search(
-        request: Request,
-        name: str,
-        sort: str = 'stars',
-        order: str = 'desc',
-        per_page: int = 30,
-        page: int = 1
-) -> dict:
-    '''Github repository search web service.
-    sortlist repository: "stars", "forks", "help-wanted-issues", "updated"
-    '''
-    return await api.github_repo_search(name=name, sort=sort, order=order, per_page=per_page, page=page)
-
-
-@app.get('/api/github-users-search', tags=['Github'], status_code=status.HTTP_200_OK)
-@app.post('/api/github-users-search', tags=['Github'], status_code=status.HTTP_200_OK)
-@limiter.limit(limit_value=LIMITER_TIME, key_func=get_remote_address)
-async def github_users_search(
-        request: Request,
-        query: str,
-        sort: str = 'followers',
-        order: str = 'desc',
-        per_page: int = 30,
-        page: int = 1,
-) -> dict:
-    '''Github users search web service.
-    sortlist repository: "followers", "repositories", "joined"
-    '''
-    return await api.github_user_search(query=query, sort=sort, order=order, per_page=per_page, page=page)
-
-
-@app.get('/api/pypi', tags=['PyPi'], status_code=status.HTTP_200_OK)
-@app.post('/api/pypi', tags=['PyPi'], status_code=status.HTTP_200_OK)
-@limiter.limit(limit_value=LIMITER_TIME, key_func=get_remote_address)
-async def pypi_package_search(request: Request, query: str) -> dict:
-    '''PyPi package search web service'''
-    return await api.pypi_package_search(query=query)
+# include router from app/router/router.py
+app.include_router(router)
