5160b2e428eb489c182151480fade2cd1c977382
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-11 21:06:48 +0330

    Update font.py

---
+++ app/router/font.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, status
+
+from typing import Optional
+import langdetect, json
+
+
+router = APIRouter(prefix='/api')
+
+@router.get('/font', tags=['Art'], status_code=status.HTTP_200_OK)
+@router.get('/font', tags=['Art'], status_code=status.HTTP_200_OK)
+async def font(text: Optional[str] = 'HeroAPI') -> dict:
+    '''Generate ascii fonts. Currently only English language is supported'''
+    if langdetect.detect(text) in ['fa', 'ar', 'ur']:
+        return {
+            'success': False, 'error_message': 'Currently, Persian language is not supported'
+        }
+    else:
+        with open('app/jsonfiles/font.json', 'r') as f:
+            fonts = json.load(f)
+
+        converted_text = str()
+        for count in range(0, len(fonts)):
+            for char in text:
+                if char.isalpha():
+                    char_index = ord(char.lower()) - 97
+                    converted_text += fonts[str(count)][char_index]
+                else:
+                    converted_text += char
+
+            converted_text += '\n'
+            final_values = converted_text.split('\n')[0:-1]
+
+        return {
+            'success': True,
+            'dev': 'Hero-Team',
+            'url': 'https://t.me/HeroAPI',
+            'github': 'https://github.com/metect/HeroAPI',
+            'data': final_values
+        }
