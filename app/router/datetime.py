ee3b96e1a0afec85610a4e9ffa31261e506e9a4b
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-11 20:54:53 +0330

    Update datetime.py

--- app/router/datetime.py
+++ app/router/datetime.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, status
 
 from jalali.Jalalian import jdate
 from typing import Optional
@@ -6,8 +6,8 @@ from typing import Optional
 
 router = APIRouter(prefix='/api')
 
-@app.get('/datetime', tags=['Date & time'], status_code=status.HTTP_200_OK)
-@app.get('/datetime', tags=['Date & time'], status_code=status.HTTP_200_OK)
+@router.get('/datetime', tags=['Date & time'], status_code=status.HTTP_200_OK)
+@router.get('/datetime', tags=['Date & time'], status_code=status.HTTP_200_OK)
 async def datetime(tr_num: Optional[str] = 'en') -> dict:
     '''Display detailed information about the date of the solar calendar'''
     current_date = jdate('H:i:s ,Y/n/j', tr_num=tr_num)
