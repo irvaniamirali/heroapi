0797519aebfca38265a120ba1e33b74b864402bc
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-12 16:45:37 +0330

    Update router.py

--- app/router/router.py
+++ app/router/router.py
@@ -5,6 +5,7 @@ from typing import Annotated, Optional
 import requests
 import bs4
 import re
+import faker
 import base64
 import codecs
 from PIL import Image
@@ -51,6 +52,41 @@ async def convert_date(day: int, month: int, year: int) -> dict:
     return {
         'success': True,
         'data': current_date
+    }
+
+
+@router.get('/faker', tags=['Fake data'], status_code=status.HTTP_200_OK)
+@router.post('/faker', tags=['Fake data'], status_code=status.HTTP_200_OK)
+async def fake_data(
+        responce: Response,
+        item: Optional[str] = 'text',
+        count: Optional[int] = '99',
+        lang: Optional[str] = 'en'
+) -> dict:
+    '''Production fake data. all items: (`text`, `name`, `email`)'''
+    if int(count) > 100:
+        responce.status_code = status.HTTP_400_BAD_REQUEST
+        return {
+            'success': False, 'error_message': 'The amount is too big. Send a smaller number `count`'
+        }
+    else:
+        final_values = list()
+        if item == 'text':
+            return {
+                'success': True,
+                'data': faker.Faker([lang]).text(count)
+            }
+        elif item == 'name':
+            for i in range(count):
+                final_values.append(faker.Faker([lang]).name())
+
+        elif item == 'email':
+            for i in range(count):
+                final_values.append(faker.Faker([lang]).email())
+
+    return {
+        'success': True,
+        'data': final_values
     }
 
 
