b2f07dc6c07f317e64419ef5214c10ee71599cf5
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-12 14:33:44 +0330

    Update pypi.py

---
+++ app/router/pypi.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, Response, status
+
+from typing import Optional
+import requests, bs4
+
+
+router = APIRouter(prefix='/api', tags=['PyPi'])
+
+@router.get('/pypi', status_code=status.HTTP_200_OK)
+@router.post('/pypi', status_code=status.HTTP_200_OK)
+async def pypi_package_search(responce: Response, query: str) -> dict:
+    '''PyPi package search web service'''
+    query = '+'.join(query.split())
+    request = requests.request(method='GET', url=f'https://pypi.org/search/?q={query}')
+    if request.status_code != requests.codes.ok:
+        responce.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {
+            'success': False, 'error_message': 'A problem has occurred on our end'
+        }
+
+    soup = bs4.BeautifulSoup(request.text, 'html.parser')
+    package_snippets = soup.find_all('a', class_='package-snippet')
+
+    search_results = list()
+    for package_snippet in package_snippets:
+        span_elems = package_snippet.find_all('span')
+        name = span_elems[0].text.strip()
+        version = span_elems[1].text.strip()
+        release_date = span_elems[2].text.strip()
+        description = package_snippet.p.text.strip()
+        search_results.append(
+            dict(
+                name=name,
+                version=version,
+                release_date=release_date,
+                description=description
+            )
+        )
+
+    return {
+        'success': True,
+        'data': search_results
+    }
