b4c169c45cce9f9d4bcab791c6b103ac63e58d8b
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-12 13:04:05 +0330

    Update app/router/bardai.py

--- app/router/bardai.py
+++ app/router/bardai.py
@@ -1,17 +1,18 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Response, status
 
 from requests import request, codes
-from typing import Optional
 
-router = APIRouter(prefix='/api')
 
-@router.get('/bardai', tags=['AI'], status_code=status.HTTP_200_OK)
-@router.get('/bardai', tags=['AI'], status_code=status.HTTP_200_OK)
-async def bard_ai(prompt: str):
+router = APIRouter(prefix='/api', tags=['AI'])
+
+@router.get('/bardai', status_code=status.HTTP_200_OK)
+@router.post('/bardai', status_code=status.HTTP_200_OK)
+async def bard_ai(responce: Response, prompt: str) -> dict:
     '''Bard artificial intelligence web service'''
     url = 'https://api.safone.dev/'
     prompt_request = request(method='GET', url=f'{url}bard?message={prompt}')
     if request.status_code != codes.ok:
+        responce.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return {
             'success': False, 'error_message': 'A problem has occurred on our end'
         }
