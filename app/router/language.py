d79530a6a898fe7932637eabbf1a810c732b7cab
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-12 13:27:06 +0330

    Update language.py

---
+++ app/router/language.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Response, status
+
+import langdetect
+
+
+router = APIRouter(prefix='/api', tags=['Language Detect'])
+
+@router.get('/lang', status_code=status.HTTP_200_OK)
+@router.post('/lang', status_code=status.HTTP_200_OK)
+async def language_detect(responce: Response, text: str) -> dict:
+    '''Identifying the language of texts'''
+    try:
+        result_detected = langdetect.detect(text)
+        return {
+            'success': True,
+            'data': result_detected
+        }
+    except langdetect.LangDetectException:
+        responce.status_code = status.HTTP_400_BAD_REQUEST
+        return {
+            'success': False,
+            'error_message': 'The value of the `text` parameter is not invalid'
+        }
