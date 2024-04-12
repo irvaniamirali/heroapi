9f96e1bd0392c5f58727157c0dc50c8bd3463e62
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-12 16:25:01 +0330

    Update router.py

--- app/router/router.py
+++ app/router/router.py
@@ -134,3 +134,28 @@ async def pypi_package_search(responce: Response, query: str) -> dict:
         'success': True,
         'data': search_results
     }
+
+
+@router.get('/icon', status_code=status.HTTP_200_OK)
+@router.post('/icon', status_code=status.HTTP_200_OK)
+async def icon(responce: Response, query: str, page: Optional[int] = 1) -> dict:
+    '''Get the icon from icon-icons.com'''
+    request = requests.request(method='GET', url=f'https://icon-icons.com/search/icons/?filtro={query}')
+    if request.status_code != requests.codes.ok:
+        responce.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {
+            'success': False, 'error_message': 'A problem has occurred on our end'
+        }
+
+    soup = bs4.BeautifulSoup(request.text, 'html.parser')
+    icons = soup.find_all('div', class_='icon-preview')
+
+    search_result = list()
+    for icon in icons:
+        data_original = icon.find('img', class_='lazy', src=True)
+        search_result.append(data_original['data-original'])
+
+    return {
+        'success': True,
+        'data': search_results
+    }
