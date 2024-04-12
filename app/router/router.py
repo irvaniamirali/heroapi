70d8a53e7a4e444480244510890a11271d0ba482
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-12 16:41:05 +0330

    Update router.py

--- app/router/router.py
+++ app/router/router.py
@@ -54,6 +54,37 @@ async def convert_date(day: int, month: int, year: int) -> dict:
     }
 
 
+@router.get('/font', tags=['Art'], status_code=status.HTTP_200_OK)
+@router.post('/font', tags=['Art'], status_code=status.HTTP_200_OK)
+async def font(responce: Response, text: Optional[str] = 'HeroAPI') -> dict:
+    '''Generate ascii fonts. Currently only English language is supported'''
+    if langdetect.detect(text) in ['fa', 'ar', 'ur']:
+        responce.status_code = status.HTTP_400_BAD_REQUEST
+        return {
+            'success': False, 'error_message': 'Currently, Persian language is not supported'
+        }
+    else:
+        with open('app/jsonfiles/font.json', 'r') as f:
+            fonts = json.load(f)
+
+        converted_text = str()
+        for count in range(0, len(fonts)):
+            for char in text:
+                if char.isalpha():
+                    char_index = ord(char.lower()) - 97
+                    converted_text += fonts[str(count)][char_index]
+                else:
+                    converted_text += char
+
+            converted_text += '\n'
+            final_values = converted_text.split('\n')[0:-1]
+
+        return {
+            'success': True,
+            'data': final_values
+        }
+
+
 @router.get('/lang', tags=['Language Detect'], status_code=status.HTTP_200_OK)
 @router.post('/lang', tags=['Language Detect'], status_code=status.HTTP_200_OK)
 async def language_detect(responce: Response, text: str) -> dict:
