b18fb418f8c3c1a228b6ff5153e30e196b4cd2d4
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-12 16:40:14 +0330

    Update router.py

--- app/router/router.py
+++ app/router/router.py
@@ -54,6 +54,24 @@ async def convert_date(day: int, month: int, year: int) -> dict:
     }
 
 
+@router.get('/lang', tags=['Language Detect'], status_code=status.HTTP_200_OK)
+@router.post('/lang', tags=['Language Detect'], status_code=status.HTTP_200_OK)
+async def language_detect(responce: Response, text: str) -> dict:
+    '''Identifying the language of texts'''
+    try:
+        result_detected = langdetect.detect(text)
+        return {
+            'success': True,
+            'data': result_detected
+        }
+    except langdetect.LangDetectException:
+        responce.status_code = status.HTTP_400_BAD_REQUEST
+        return {
+            'success': False,
+            'error_message': 'The value of the `text` parameter is not invalid'
+        }
+
+
 @router.get('/github-topic-search', tags=['Github'], status_code=status.HTTP_200_OK)
 @router.post('/github-topic-search', tags=['Github'], status_code=status.HTTP_200_OK)
 async def github_topic_search(
