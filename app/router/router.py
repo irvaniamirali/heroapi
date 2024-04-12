805cd022592728689dcbb2d33d8a6b0f8e50dddc
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-12 16:38:24 +0330

    Update router.py

--- app/router/router.py
+++ app/router/router.py
@@ -12,6 +12,26 @@ from PIL import Image
 
 router = APIRouter(prefix='/api')
 
+@router.get('/bard', tags=['AI'], status_code=status.HTTP_200_OK)
+@router.post('/bard', tags=['AI'], status_code=status.HTTP_200_OK)
+async def bard_ai(responce: Response, prompt: str) -> dict:
+    '''Bard artificial intelligence web service'''
+    url = 'https://api.safone.dev/'
+    prompt_request = request(method='GET', url=f'{url}bard?message={prompt}')
+    if request.status_code != codes.ok:
+        responce.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {
+            'success': False, 'error_message': 'A problem has occurred on our end'
+        }
+
+    responce = prompt_request.json()
+    final_responce = responce['candidates'][0]['content']['parts'][0]['text']
+    return {
+        'success': True,
+        'data': final_responce
+    }
+
+
 @router.get('/github-topic-search', tags=['Github'], status_code=status.HTTP_200_OK)
 @router.post('/github-topic-search', tags=['Github'], status_code=status.HTTP_200_OK)
 async def github_topic_search(
