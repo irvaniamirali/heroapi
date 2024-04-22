080b6850e69853ae9f7f4a00e0b1bd2dac4b8072
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-22 16:31:26 +0330

    Update dictionary.py

---
+++ app/router/dictionary.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Response, status
+
+import requests
+import bs4
+
+router = APIRouter(prefix='/api', tags=['Dictionary'])
+
+
+@router.get('/dict', status_code=status.HTTP_200_OK)
+@router.post('/dict', status_code=status.HTTP_200_OK)
+async def dictionary(responce: Response, query: str) -> dict:
+    '''Search words in deh khoda dictionary'''
+    request = requests.request(
+        method='GET', url=f'https://dehkhoda.ut.ac.ir/fa/dictionary/{query}'
+    )
+    if request.status_code != requests.codes.ok:
+        responce.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {
+            'success': False, 'error_message': 'A problem has occurred on our end'
+        }
+
+    soup = bs4.BeautifulSoup(request.text, 'html.parser')
+    paragraphs = soup.find_all('div', class_='definitions p-t-1')
+    return {
+        'success': True,
+        'data': paragraphs[0].text
+    }
