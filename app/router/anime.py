4052ece857edbf99671d8a183c2f5ff7351c62ce
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-24 10:49:21 +0330

    Update anime.py

--- app/router/anime.py
+++ app/router/anime.py
@@ -10,7 +10,7 @@ router = APIRouter(prefix='/api', tags=['Anime'])
 @router.post('/rand-anime', status_code=status.HTTP_200_OK)
 async def random_anime_image(responce: Response) -> "FileResponse":
     '''Get the icon from icon-icons.com'''
-    request = requests.request(method='GET', url='https://pic.re/image')
+    request = requests.request(method='POST', url='https://pic.re/image')
     if request.status_code != requests.codes.ok:
         responce.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return {
