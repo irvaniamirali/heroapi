cde8025f95e9d5a14b088fdabbe1a3feb4a2ee0e
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-26 19:44:36 +0330

    Update _github.py

--- app/router/_github.py
+++ app/router/_github.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, status
 
 from typing import Optional
 import requests
@@ -13,21 +13,10 @@ headers = {
 
 @router.get('/github-topic-search', status_code=status.HTTP_200_OK)
 @router.post('/github-topic-search', status_code=status.HTTP_200_OK)
-async def github_topic_search(
-        responce: Response,
-        query: str,
-        per_page: Optional[int] = 30,
-        page: Optional[int] = 1
-) -> dict:
+async def github_topic_search(query: str, per_page: Optional[int] = 30, page: Optional[int] = 1) -> dict:
     '''Github topic search web service'''
     query_url = 'topics?q=%s&per_page=%s&page=%s'
     request = requests.request(method='GET', url=query_url % (query, per_page, page), headers=headers)
-    if request.status_code != requests.codes.ok:
-        responce.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {
-            'success': False, 'error_message': 'A problem has occurred on our end'
-        }
-
     return {
         'success': True,
         'data': request.json()
@@ -37,7 +26,6 @@ async def github_topic_search(
 @router.get('/github-repo-search', status_code=status.HTTP_200_OK)
 @router.post('/github-repo-search', status_code=status.HTTP_200_OK)
 async def github_repo_search(
-        responce: Response,
         name: str,
         sort: Optional[str] = 'stars',
         order: Optional[str] = 'desc',
@@ -49,13 +37,8 @@ async def github_repo_search(
     '''
     query_url = base_url + 'repositories?q=%s&s=%s&order=%s&per_page=%s&page=%s'
     request = requests.request(
-        method='GET', url=query_url % (name, sort, order, per_page, page), headers=headers
+        method='POST', url=query_url % (name, sort, order, per_page, page), headers=headers
     )
-    if request.status_code != requests.codes.ok:
-        responce.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {
-            'success': False, 'error_message': 'A problem has occurred on our end'
-        }
 
     return {
         'success': True,
@@ -66,7 +49,6 @@ async def github_repo_search(
 @router.get('/github-users-search', status_code=status.HTTP_200_OK)
 @router.post('/github-users-search', status_code=status.HTTP_200_OK)
 async def github_users_search(
-        responce: Response,
         query: str,
         sort: Optional[str] = 'followers',
         order: Optional[str] = 'desc',
@@ -80,12 +62,6 @@ async def github_users_search(
     request = requests.request(
         method='GET', url=query_url % (query, sort, order, per_page, page), headers=headers
     )
-    if request.status_code != requests.codes.ok:
-        responce.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {
-            'success': False, 'error_message': 'A problem has occurred on our end'
-        }
-
     return {
         'success': True,
         'data': request.json()
