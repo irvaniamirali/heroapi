2266c0b68d0d1b1a2903d7b146b3359c83a79d61
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-26 20:57:20 +0330

    Update dictionary.py

--- app/router/dictionary.py
+++ app/router/dictionary.py
@@ -2,23 +2,18 @@ from fastapi import APIRouter, Response, status
 
 import requests
 import bs4
+import re
 
 router = APIRouter(prefix='/api', tags=['Dictionary'])
 
 
 @router.get('/dict/v1', status_code=status.HTTP_200_OK)
 @router.post('/dict/v1', status_code=status.HTTP_200_OK)
-async def dictionary(responce: Response, query: str) -> dict:
+async def dictionary(query: str) -> dict:
     '''Search words in deh khoda dictionary'''
     request = requests.request(
-        method='GET', url=f'https://dehkhoda.ut.ac.ir/fa/dictionary/{query}'
+        method='POST', url=f'https://dehkhoda.ut.ac.ir/fa/dictionary/{query}'
     )
-    if request.status_code != requests.codes.ok:
-        responce.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {
-            'success': False, 'error_message': 'A problem has occurred on our end'
-        }
-
     soup = bs4.BeautifulSoup(request.text, 'html.parser')
     paragraphs = soup.find('div', class_='definitions p-t-1')
     return {
@@ -34,21 +29,20 @@ async def dictionary(responce: Response, query: str) -> dict:
     request = requests.request(
         method='GET', url=f'https://vajehyab.com/amid/{query}'
     )
-    if request.status_code != requests.codes.ok:
-        responce.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {
-            'success': False, 'error_message': 'A problem has occurred on our end'
-        }
-
     soup = bs4.BeautifulSoup(request.text, 'html.parser')
-    paragraphs = soup.find('div', class_='_51HBSo', role='definition')
-    if paragraphs is None:
+    paragraph_tag = soup.find('div', class_='_51HBSo', role='definition')
+    if paragraph_tag is None:
+        responce.status_code = status.HTTP_400_BAD_REQUEST
         return {
             'success': False,
             'error_message': 'Your word was not found in the dictionary'
         }
 
+    paragraphs = re.findall(
+        r'^<div\ class=\".*\"\ role=\"definition\">(.*?)</div>$', str(paragraph_tag)
+    )
+    clean_paragraphs = paragraphs[0].split('<br/>')
     return {
         'success': True,
-        'data': paragraphs.text
+        'data': clean_paragraphs
     }
