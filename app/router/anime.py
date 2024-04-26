04cc20dabe70907f18074fcebb8ae564a3b16834
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-26 19:47:33 +0330

    Update anime.py

--- app/router/anime.py
+++ app/router/anime.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, status
 from fastapi.responses import FileResponse
 
 import requests
@@ -8,15 +8,9 @@ router = APIRouter(prefix='/api', tags=['Anime'])
 
 @router.get('/rand-anime', status_code=status.HTTP_200_OK)
 @router.post('/rand-anime', status_code=status.HTTP_200_OK)
-async def random_anime_image(responce: Response) -> "FileResponse":
+async def random_anime_image() -> "FileResponse":
     '''Get the icon from icon-icons.com'''
     request = requests.request(method='POST', url='https://pic.re/image')
-    if request.status_code != requests.codes.ok:
-        responce.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {
-            'success': False, 'error_message': 'A problem has occurred on our end'
-        }
-
     with open('app/tmpfiles/anime.png', 'wb+') as _file:
         _file.write(request.content)
 
