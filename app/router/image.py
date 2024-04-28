9f7a708ae7e09f817418669cd4ce351dc95e7aab
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-28 19:42:24 +0330

    Update image.py

--- app/router/image.py
+++ app/router/image.py
@@ -2,10 +2,40 @@ from fastapi import APIRouter, File, status
 from fastapi.responses import FileResponse
 
 from typing import Annotated
+
+from bs4 import BeautifulSoup
 from PIL import Image
+import requests
 import re
 
-router = APIRouter(prefix='/api', tags=['Image'])
+router = APIRouter(prefix='/api')
+
+
+@router.get('/icon', tags=['Icon Search'], status_code=status.HTTP_200_OK)
+@router.post('/icon', tags=['Icon Search'], status_code=status.HTTP_200_OK)
+async def icon(responce: Response, query: str, page: Optional[int] = 1) -> dict:
+    '''Get the icon from icon-icons.com'''
+    request = requests.request(
+        method='GET', url=f'https://icon-icons.com/search/icons/?filtro={query}&page={page}'
+    )
+    if request.status_code != requests.codes.ok:
+        responce.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {
+            'success': False, 'error_message': 'A problem has occurred on our end'
+        }
+
+    soup = BeautifulSoup(request.text, 'html.parser')
+    icons = soup.find_all('div', class_='icon-preview')
+
+    search_result = list()
+    for icon in icons:
+        data_original = icon.find('img', class_='lazy', src=True)
+        search_result.append(data_original.get('data-original'))
+
+    return {
+        'success': True,
+        'data': search_result
+    }
 
 
 @router.get('/png2ico', tags=['Image'], status_code=status.HTTP_200_OK)
