e703d33aa3c51abb5e24cef28b763247a90ba261
amirali irvany <irvanyamirali@gmail.com>
2024-05-10 20:49:30 +0330

    Update other.py

--- app/router/api/other.py
+++ app/router/api/other.py
@@ -40,20 +40,6 @@ async def icon(responce: Response, query: str, page: Optional[int] = 1) -> dict:
     }
 
 
-@router.get('/png2ico', tags=['Image'], status_code=status.HTTP_200_OK)
-@router.post('/png2ico', tags=['Image'], status_code=status.HTTP_200_OK)
-async def convert_image_to_ico_format(image: Annotated[bytes, File()]):
-    '''Convert image in png format to ico'''
-    FILE_PATH = 'app/tmpfiles/logo.png'
-    with open(FILE_PATH, 'wb') as _file:
-        _file.write(image)
-
-    logo = Image.open(FILE_PATH)
-    ICO_FILE_PATH = re.sub('png', 'ico', FILE_PATH)
-    logo.save(ICO_FILE_PATH, format='ico')
-    return FileResponse(ICO_FILE_PATH)
-
-
 @router.get('/lang', tags=['Language Detect'], status_code=status.HTTP_200_OK)
 @router.post('/lang', tags=['Language Detect'], status_code=status.HTTP_200_OK)
 async def language_detect(responce: Response, text: str) -> dict:
@@ -70,7 +56,6 @@ async def language_detect(responce: Response, text: str) -> dict:
             'success': False,
             'error_message': 'The value of the `text` parameter is not invalid'
         }
-
 
 
 @router.get('/food/v1', status_code=status.HTTP_200_OK)
