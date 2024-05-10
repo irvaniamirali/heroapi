77755d1e8f40604a170ed22301102aa4f1cb5087
amirali irvany <irvanyamirali@gmail.com>
2024-05-10 20:41:22 +0330

    Update router.py

--- app/router/router.py
+++ app/router/router.py
@@ -1,5 +1,4 @@
 
-
 class Routers:
 
     def __init__(self, app, routes: list):
@@ -11,14 +10,14 @@ class Routers:
         return self
 
     def _create_route_methods(self):
-          for route in self.routes:
-              module_path, route_name = route.rsplit('.', maxsplit=1)
-              module = __import__(module_path, fromlist=[route_name])
-              route_module = getattr(module, route_name)
-              route_method_name = f'{route_name.title().replace("_", "")}Route'
+        for route in self.routes:
+            module_path, route_name = route.rsplit('.', maxsplit=1)
+            module = __import__(module_path, fromlist=[route_name])
+            route_module = getattr(module, route_name)
+            route_method_name = f'{route_name.title().replace("_", "")}Route'
 
-              def route_method():
-                  return self.app.include_router(route_module)
+            def route_method():
+                return self.app.include_router(route_module)
 
-              setattr(self, route_method_name, route_method)
-              getattr(self, route_method_name)()
+            setattr(self, route_method_name, route_method)
+            getattr(self, route_method_name)()
