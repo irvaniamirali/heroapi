8791fea0c618745abf7c2f08fd4f92ce80c9ed24
AmirAli Irvany <irvanyamirali@gmail.com>
2024-05-24 19:20:01 +0330

    Update _captcha.py

--- app/router/api/_captcha.py
+++ app/router/api/_captcha.py
@@ -7,7 +7,7 @@ from captcha.image import ImageCaptcha
 router = APIRouter(prefix='/api', tags=['Captcha'])
 
 @router.get('/captcha', status_code=status.HTTP_200_OK)
-@router.post('captcha', status_code=status.HTTP_200_OK)
+@router.post('/captcha', status_code=status.HTTP_200_OK)
 async def generateCaptcha(text: str, width: int = 280, height: int = 90):
     # Create an image instance of the given size
     image = ImageCaptcha(width = width, height = height)
