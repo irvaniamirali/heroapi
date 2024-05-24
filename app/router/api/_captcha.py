c5f326b367248e57045184e0ded9935f6c809201
amirali irvany <irvanyamirali@gmail.com>
2024-05-24 16:02:40 +0330

    Update `captcha` api

--- app/router/api/_captcha.py
+++ app/router/api/_captcha.py
@@ -1,23 +1,17 @@
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, status
 from fastapi.responses import FileResponse
 
-# Import the following modules
+from typing import Optional
+
 from captcha.image import ImageCaptcha
 
 router = APIRouter(prefix='/api', tags=['Captcha'])
 
+
 @router.get('/captcha', status_code=status.HTTP_200_OK)
 @router.post('captcha', status_code=status.HTTP_200_OK)
-async def generateCaptcha(text: str, width: int = 280, height: int = 90):
-    # Create an image instance of the given size
+async def generateCaptcha(text: str, width: Optional[int] = 280, height: Optional[int] = 90):
     image = ImageCaptcha(width = width, height = height)
-
-    # Image captcha text
-    captcha_text = text
-
-    # generate the image of the given text
-    data = image.generate(captcha_text) 
-
-    # write the image on the given file and save it
-    image.write(captcha_text, 'CAPTCHA.png')
+    data = image.generate(text)
+    image.write(text, 'CAPTCHA.png')
     return FileResponse("CAPTCHA.png")
