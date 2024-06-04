5cd5667824756730806a79440771e496363e05cb
amirali irvany <irvanyamirali@gmail.com>
2024-06-04 13:56:21 +0330

    Fix bugs

--- app/router/api/_captcha.py
+++ app/router/api/_captcha.py
@@ -11,7 +11,10 @@ router = APIRouter(prefix='/api', tags=['Captcha'])
 @router.get('/captcha', status_code=status.HTTP_200_OK)
 @router.post('captcha', status_code=status.HTTP_200_OK)
 async def generate_captcha(text: str, width: Optional[int] = 280, height: Optional[int] = 90):
-    image = ImageCaptcha(width = width, height = height)
-    data = image.generate(text)
-    image.write(text, 'CAPTCHA.png')
-    return FileResponse("CAPTCHA.png")
+    '''Generate captcha image'''
+    image = ImageCaptcha(width=width, height=height)
+
+    FILE_PAHT = 'app/tmp/captcha.png'
+
+    image.write(text, FILE_PAHT)
+    return FileResponse(FILE_PAHT)
