10b557e87830451a6f34dced3bfb087215489606
AmirAli Irvany <irvanyamirali@gmail.com>
2024-06-27 15:10:16 +0330

    Update captcha.py

--- app/router/api/_captcha.py
+++ app/router/api/_captcha.py
@@ -5,16 +5,17 @@ from typing import Optional
 
 from captcha.image import ImageCaptcha
 
-router = APIRouter(prefix='/api', tags=['Captcha'])
+router = APIRouter(prefix="/api", tags=["Captcha"])
+
+FILE_PATH = "app/tmp/captcha.png"
 
 
-@router.get('/captcha', status_code=status.HTTP_200_OK)
-@router.post('captcha', status_code=status.HTTP_200_OK)
+@router.get("/captcha", status_code=status.HTTP_200_OK)
+@router.post("captcha", status_code=status.HTTP_200_OK)
 async def generate_captcha(text: str, width: Optional[int] = 280, height: Optional[int] = 90):
-    '''Generate captcha image'''
+    """
+    Generate captcha image
+    """
     image = ImageCaptcha(width=width, height=height)
-
-    FILE_PAHT = 'app/tmp/captcha.png'
-
-    image.write(text, FILE_PAHT)
-    return FileResponse(FILE_PAHT)
+    image.write(text, FILE_PATH)
+    return FileResponse(FILE_PATH)
