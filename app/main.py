a12a96cc510f72ece00ff1608bf36a9ff513e226
AmirAli Irvany <irvanyamirali@gmail.com>
2024-06-27 15:01:46 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -29,6 +29,9 @@ app.mount("/app/static", StaticFiles(directory="app/static"), name="static")
 
 @app.get("/docs", include_in_schema=False)
 async def swagger_ui_html():
+    """
+    Return swagger API document
+    """
     return get_swagger_ui_html(
         openapi_url="/openapi.json",
         title="HeroAPI",
