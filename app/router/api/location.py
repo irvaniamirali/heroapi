16fc61b663e31cb570ff27ce9ea2c9361e9ef22f
AmirAli Irvani <irvaniamirali@proton.me>
2024-07-08 16:35:06 +0330

    Update location.py

--- app/router/api/location.py
+++ app/router/api/location.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Response, status
 
 from typing import Optional
-import requests
+
+import httpx
 import os
 
 router = APIRouter(prefix="/api", tags=["Location"])
@@ -22,18 +23,19 @@ async def location(
 
     url = "https://api.neshan.org/v1/"
     query_url = f"{url}search?term={text}&lat={latitude}&lng={longitude}"
-    request = requests.request(
+    req = httpx.request(
         method="POST", url=query_url, headers={
             "Api-Key": access_key
         }
     )
-    if request.status_code != requests.codes.ok:
+    if req.status_code != httpx.codes.OK:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return {
-            "success": False, "error_message": "A problem has occurred on our end"
+            "success": False,
+            "error_message": "A problem has occurred on our end"
         }
 
     return {
         "success": True,
-        "data": request.json()
+        "data": req.json()
     }
