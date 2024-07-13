459ca23406545bc7ebbcf13a87d4ac712398f9b1
AmirAli Irvani <irvaniamirali@proton.me>
2024-07-13 17:16:14 +0330

    Update datetime.py

--- app/router/api/datetime.py
+++ app/router/api/datetime.py
@@ -3,21 +3,25 @@ from fastapi import APIRouter, Response, status
 from typing import Optional
 
 from jalali.Jalalian import jdate
+
 import jdatetime
 
-router = APIRouter(prefix="/api", tags=["Date & time"])
+tags = ["Date & time"]
+
+router = APIRouter(tags=tags)
 
 
 @router.get("/datetime", status_code=status.HTTP_200_OK)
 @router.post("/datetime", status_code=status.HTTP_200_OK)
-async def date_time(number_lang: str = "fa") -> dict:
+async def date_time(number_lang: str = "en") -> dict:
     """
     Date and time display web service
     """
     current_date = jdate("H:i:s ,Y/n/j", tr_num=number_lang)
     return {
         "success": True,
-        "data": current_date
+        "data": current_date,
+        "error_message": None
     }
 
 
@@ -27,8 +31,9 @@ async def convert_date(day: int, month: int, year: int) -> dict:
     """
     Convert Shamsi date to Gregorian
     """
-    result_date = jdatetime.date(day=day, month=month, year=year).togregorian()
+    date = jdatetime.date(day=day, month=month, year=year).togregorian()
     return {
         "success": True,
-        "data": result_date
+        "data": date,
+        "error_message": None
     }
