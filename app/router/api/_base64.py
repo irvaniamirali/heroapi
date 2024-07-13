3c2d99b179db63f6c36752c4fb56d8818430a754
AmirAli Irvani <irvaniamirali@proton.me>
2024-07-13 16:53:18 +0330

    Update base64.py

--- app/router/api/_base64.py
+++ app/router/api/_base64.py
@@ -3,33 +3,44 @@ from fastapi import APIRouter, Response, status
 import codecs
 import base64
 
+from binascii import Error
+
 router = APIRouter(prefix="/api", tags=["Base64"])
 
 
 @router.get("/bs64encode", status_code=status.HTTP_200_OK)
 @router.post("/bs64encode", status_code=status.HTTP_200_OK)
 async def base64encode(text: str) -> dict:
+    """
+    Encode to Base64 format
+    """
     b_string = codecs.encode(text, "utf-8")
     output = base64.b64encode(b_string)
     return {
         "success": True,
-        "data": output
+        "data": output,
+        "error_message": None
     }
 
 
 @router.get("/bs64decode", status_code=status.HTTP_200_OK)
 @router.post("/bs64decode", status_code=status.HTTP_200_OK)
 async def b64decode(response: Response, text: str) -> dict:
+    """
+    Decode from Base64 format
+    """
     b_string = codecs.encode(text, "utf-8")
     try:
         output = base64.b64decode(b_string)
         return {
             "success": True,
-            "data": output
+            "data": output,
+            "error_message": None
         }
-    except:
+    except Error:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return {
             "success": False,
+            "data": None,
             "error_message": "This text not base64"
         }
