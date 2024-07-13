0d20f7d8133c20165d0d65a02d10251c6f5ac396
AmirAli Irvani <irvaniamirali@proton.me>
2024-07-13 18:42:34 +0330

    Update store.py

--- app/router/api/store.py
+++ app/router/api/store.py
@@ -7,7 +7,7 @@ import httpx
 
 client = httpx.AsyncClient()
 
-router = APIRouter(prefix="/api", tags=["Store"])
+router = APIRouter(tags=["Store"])
 
 
 @router.get("/divar", status_code=status.HTTP_200_OK)
@@ -16,19 +16,20 @@ async def divar(response: Response, query: str, city: Optional[str] = "tehran") 
     """
     Web search service in [Divar](https://divar.ir)
     """
-    req = await client.request(method="GET", url=f"https://divar.ir/s/{city}?q={query}")
-    if req.status_code != httpx.codes.OK:
+    request = await client.request(method="GET", url=f"https://divar.ir/s/{city}?q={query}")
+    if request.status_code != httpx.codes.OK:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return {
             "success": False,
+            "data": None,
             "error_message": "A problem has occurred on our end"
         }
 
-    req = req.text
-    start, finish = req.rfind("["), req.rfind("]")
+    request = request.text
+    start, finish = request.rfind("["), request.rfind("]")
 
     values = str()
-    computed_value = list(req)[start:finish]
+    computed_value = list(request)[start:finish]
     for index in range(len(computed_value)):
         values += computed_value[index]
 
@@ -36,5 +37,6 @@ async def divar(response: Response, query: str, city: Optional[str] = "tehran") 
     final_values = literal_eval(node_or_string=values)
     return {
         "success": True,
-        "data": final_values
+        "data": final_values,
+        "error_message": None
     }
