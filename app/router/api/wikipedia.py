bf3af84a974fba03a253f8a58ac2eac754007205
AmirAli Irvani <irvaniamirali@proton.me>
2024-07-30 21:29:45 +0330

    Update wikipedia.py

--- app/router/api/wikipedia.py
+++ app/router/api/wikipedia.py
@@ -1,28 +1,37 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Response, status
 
 from typing import Optional
 
-import wikipediaapi
+from httpx import AsyncClient, codes
+
+client = AsyncClient()
 
 router = APIRouter(prefix="/wikipedia", tags=["Wikipedia Search"])
 
 
 @router.get("/search", status_code=status.HTTP_200_OK)
 @router.post("/search", status_code=status.HTTP_200_OK)
-async def wikipedia_search(query: str, results: Optional[int] = 10, lang: Optional[str] = "en"):
+async def wikipedia_search(
+        response: Response,
+        query: str,
+        lang: Optional[str] = "en",
+        format: Optional[str] = "json"
+):
     """
     Do a Wikipedia search for `query`.
     """
-    wiki = wikipediaapi.Wikipedia("HeroAPI (username@example.com)", lang)
-    page = wiki.page(title=query)
+    base_url = "https://%s.wikipedia.org/w/api.php?action=parse&page=%s&format=%s"
+    request = await client.request(method="GET", url=base_url % (lang, query, format))
+    if request.status_code != codes.OK:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {
+            "success": False,
+            "data": None,
+            "error_message": "A problem has occurred on our end"
+        }
+
     return {
         "success": True,
-        "data": {
-            "exists": page.exists(),
-            "title": page.title,
-            "summary": page.summary,
-            "text": page.text,
-            "language": page.language,
-        },
-        "error_message": None
+        "data": request.json(),
+        "error_message": False
     }
