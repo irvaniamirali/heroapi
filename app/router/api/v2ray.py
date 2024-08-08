26cea33a9e4a81ae6ad815f832802a9190880f79
AmirAli Irvani <irvaniamirali@proton.me>
2024-08-09 00:31:02 +0330

    Update v2ray.py

---
+++ app/router/api/v2ray.py
@@ -0,0 +1,64 @@
+from fastapi import APIRouter, status
+
+from typing import Optional
+
+import random
+import httpx
+
+client = httpx.AsyncClient()
+
+router = APIRouter(tags=["V2ray config"])
+
+
+async def http_request(url: str, method: str = "GET", max_retry: int = 3):
+    """
+    Execute asynchronous request to GitHub
+    """
+    for _ in range(max_retry):
+        response = await client.request(method=method, url=url)
+        if response.status_code == httpx.codes.OK:
+            return response.text
+
+
+# https://github.com/barry-far/V2ray-Configs
+URL = "https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/All_Configs_Sub.txt"
+
+
+@router.get("/v2ray", status_code=status.HTTP_200_OK)
+@router.post("/v2ray", status_code=status.HTTP_200_OK)
+async def v2ray(num_results: Optional[int] = 10):
+    """
+    Get free v2ray configs (any types)
+    """
+    response = await http_request(URL)
+    configs = response.splitlines()
+
+    for config in configs:
+        if config.startswith("#"):
+            index = configs.index(config)
+            del configs[index]
+
+    if num_results:
+        unique_configs = []
+        for config in range(num_results):
+            if config not in configs:
+                random_index = random.randint(0, len(configs))
+                unique_configs.append(configs[random_index])
+
+        return {
+            "success": True,
+            "data": {
+                "configs": unique_configs,
+                "count": len(unique_configs),
+            },
+            "error_message": None
+        }
+
+    return {
+        "success": True,
+        "data": {
+            "configs": configs,
+            "count": len(configs),
+        },
+        "error_message": None
+    }
