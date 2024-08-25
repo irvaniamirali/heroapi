a0280201079736c0a533d93fbca8513d4c12c276
AmirAli Irvani <irvaniamirali@proton.me>
2024-08-25 22:06:39 +0330

    Update router.py

--- app/router/router.py
+++ app/router/router.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter, Response, status
 
+import asyncio
+
+from typing import Optional
+
 from app.router.api import ai
+from app.router.api import duckduckgo
 
 router = APIRouter(prefix="/api")
 
@@ -9,6 +14,25 @@ router = APIRouter(prefix="/api")
 @router.post("/gpt", status_code=status.HTTP_200_OK)
 async def gpt(response: Response, query: str) -> dict:
     """
-    ChatGPT 3.5 API
+    ChatGPT 3.5 API.
     """
     return await ai.gpt(response, query)
+
+
+@router.get("/duckduckgo/text", status_code=status.HTTP_200_OK)
+@router.post("/duckduckgo/text", status_code=status.HTTP_200_OK)
+async def duckduckgo_text(
+        query: str,
+        max_results: Optional[int] = 100,
+        region: Optional[str] = "wt-wt",
+        limit: Optional[str] = None
+) -> dict:
+    """
+    DuckDuckGo text search. Query params: https://duckduckgo.com/params.
+    """
+    query_results = await asyncio.gather(duckduckgo.text_query(query, max_results, region, limit))
+    return {
+        "success": True,
+        "data": query_results,
+        "error_message": None
+    }
