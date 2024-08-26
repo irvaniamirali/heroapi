017a284b55e4aeb9ea5e5cf80d9db7dc271768cf
AmirAli Irvani <irvaniamirali@proton.me>
2024-08-26 15:53:47 +0330

    Update base64_api.py

--- app/router/api/base64_api.py
+++ app/router/api/base64_api.py
@@ -1,46 +1,28 @@
-from fastapi import APIRouter, Response, status
+from fastapi import status
 
 import codecs
 import base64
 
 from binascii import Error
 
-router = APIRouter(tags=["Base64"])
 
-
-@router.get("/bs64encode", status_code=status.HTTP_200_OK)
-@router.post("/bs64encode", status_code=status.HTTP_200_OK)
-async def base64encode(text: str) -> dict:
+async def base64encode(text):
     """
     Encode to Base64 format
     """
     string = codecs.encode(text, "utf-8")
-    output = base64.b64encode(string)
-    return {
-        "success": True,
-        "data": output,
-        "error_message": None
-    }
+    return base64.b64encode(string)
 
 
-@router.get("/bs64decode", status_code=status.HTTP_200_OK)
-@router.post("/bs64decode", status_code=status.HTTP_200_OK)
-async def b64decode(response: Response, text: str) -> dict:
+async def b64decode(response, string, payload):
     """
     Decode from Base64 format
     """
-    string = codecs.encode(text, "utf-8")
+    string = codecs.encode(string, "utf-8")
     try:
         output = base64.b64decode(string)
-        return {
-            "success": True,
-            "data": output,
-            "error_message": None
-        }
+        payload["data"] = output
     except Error:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {
-            "success": False,
-            "data": None,
-            "error_message": "This text not base64"
-        }
+        payload["success"] = False
+        payload["error_message"] = "This text is not base64"
