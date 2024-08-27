cb0bf118b2cf626dda23e19a1ac1d0d50d036f44
AmirAli Irvani <irvaniamirali@proton.me>
2024-08-27 12:59:28 +0330

    Update datetime_api.py

--- app/router/api/datetime_api.py
+++ app/router/api/datetime_api.py
@@ -6,7 +6,11 @@ async def iso_date(year, month, day, prefix):
     date_result = JalaliDate(year, month, day).isoformat()
     if prefix:
         date_result = JalaliDate(year, month, day).strftime(f"%Y{prefix}%m{prefix}%d")
-    return date_result
+    return {
+        "success": True,
+        "data": date_result,
+        "error_message": None
+    }
 
 
 async def solar_date():
@@ -15,20 +19,24 @@ async def solar_date():
     """
     current_date = JalaliDateTime.now()
     return {
-        "current": str(current_date.now()),
-        "time": {
-            "full": current_date.time(),
-            "hour": current_date.hour,
-            "minute": current_date.minute,
-            "second": current_date.second,
-            "microsecond": current_date.microsecond
+        "success": True,
+        "data": {
+            "current": str(current_date.now()),
+            "time": {
+                "full": current_date.time(),
+                "hour": current_date.hour,
+                "minute": current_date.minute,
+                "second": current_date.second,
+                "microsecond": current_date.microsecond
+            },
+            "date": {
+                "full": str(current_date.date()),
+                "day": current_date.day,
+                "month": current_date.month,
+                "year": current_date.year,
+            }
         },
-        "date": {
-            "full": str(current_date.date()),
-            "day": current_date.day,
-            "month": current_date.month,
-            "year": current_date.year,
-        }
+        "error_message": None
     }
 
 
@@ -37,30 +45,41 @@ async def ad_date():
     Current AD date
     """
     current_date = datetime.now()
-    date_result = {
-        "current": current_date,
-        "time": {
-            "full": current_date.time(),
-            "hour": current_date.hour,
-            "minute": current_date.minute,
-            "second": current_date.second,
-            "microsecond": current_date.microsecond
+    return {
+        "success": True,
+        "data": {
+            "current": current_date,
+            "time": {
+                "full": current_date.time(),
+                "hour": current_date.hour,
+                "minute": current_date.minute,
+                "second": current_date.second,
+                "microsecond": current_date.microsecond
+            },
+            "date": {
+                "full": current_date.date(),
+                "day": current_date.day,
+                "month": current_date.month,
+                "year": current_date.year,
+            }
         },
-        "date": {
-            "full": current_date.date(),
-            "day": current_date.day,
-            "month": current_date.month,
-            "year": current_date.year,
-        }
+        "error_message": None
     }
-    return date_result
 
 
 async def convert_ad_to_jalali(year, month, day):
     date_result = JalaliDate(date(year, month, day))
-    return date_result
+    return {
+        "success": True,
+        "data": date_result,
+        "error_message": None
+    }
 
 
 async def convert_jalali_to_ad(year, month, day):
     date_result = JalaliDate.to_jalali(year, month, day)
-    return date_result
+    return {
+        "success": True,
+        "data": date_result,
+        "error_message": None
+    }
