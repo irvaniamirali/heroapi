cf0095578e3806273d7b66e5e1882c4095678b93
AmirAli Irvani <irvaniamirali@proton.me>
2024-08-27 18:21:16 +0330

    Update github.py

--- app/router/api/github.py
+++ app/router/api/github.py
@@ -1,48 +1,25 @@
-from fastapi import APIRouter, Response, status
+from httpx import AsyncClient
 
-from typing import Optional
+client = AsyncClient()
 
-import httpx
+BASE_URL = "https://api.github.com/search/"
 
-client = httpx.AsyncClient()
-
-router = APIRouter(prefix="/github", tags=["GitHub"])
-
-base_url = "https://api.github.com/search/"
+HEADERS = {
+    "Accept": "application/vnd.github+json"
+}
 
 
 async def github_search(path):
     """
     Execute asynchronous request to GitHub API
     """
-    headers: dict = {
-        "Accept": "application/vnd.github+json"
-    }
-    response = await client.request(method="GET", url=path, headers=headers)
+    response = await client.request(method="GET", url=path, headers=HEADERS)
     return response
 
 
-@router.get("/topic", status_code=status.HTTP_200_OK)
-@router.post("/topic", status_code=status.HTTP_200_OK)
-async def github_topic_search(
-        response: Response,
-        query: str,
-        per_page: Optional[int] = 30,
-        page: Optional[int] = 1
-) -> dict:
-    """
-    GitHub topic search web service
-    """
-    path = base_url + "topics?q=%s&per_page=%s&page=%s" % (query, per_page, page)
+async def topic_search(query, per_page, page):
+    path = BASE_URL + "topics?q=%s&per_page=%s&page=%s" % (query, per_page, page)
     request = await github_search(path=path)
-    if request.status_code != httpx.codes.OK:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {
-            "success": False,
-            "data": None,
-            "error_message": "A problem has occurred on our end"
-        }
-
     return {
         "success": True,
         "data": request.json(),
@@ -50,61 +27,21 @@ async def github_topic_search(
     }
 
 
-@router.get("/repo", status_code=status.HTTP_200_OK)
-@router.post("/repo", status_code=status.HTTP_200_OK)
-async def github_repo_search(
-        response: Response,
-        name: str,
-        sort: Optional[str] = "stars",
-        order: Optional[str] = "desc",
-        per_page: Optional[int] = 30,
-        page: Optional[int] = 1
-) -> dict:
-    """
-    GitHub repository search web service.
-    sortlist repository: "stars", "forks", "help-wanted-issues", "updated"
-    """
-    path = base_url + "repositories?q=%s&s=%s&order=%s&per_page=%s&page=%s"
+async def repo_search(name, sort, order, per_page, page):
+    path = BASE_URL + "repositories?q=%s&s=%s&order=%s&per_page=%s&page=%s"
     request = await github_search(path=path % (name, sort, order, per_page, page))
-    if request.status_code != httpx.codes.OK:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {
-            "success": False,
-            "data": None,
-            "error_message": "A problem has occurred on our end",
-        }
-
     return {
         "success": True,
-        "data": request.json()
+        "data": request.json(),
+        "error_message": None
     }
 
 
-@router.get("/users", status_code=status.HTTP_200_OK)
-@router.post("/users", status_code=status.HTTP_200_OK)
-async def github_users_search(
-        response: Response,
-        query: str,
-        sort: Optional[str] = "followers",
-        order: Optional[str] = "desc",
-        per_page: Optional[int] = 30,
-        page: Optional[int] = 1,
-) -> dict:
-    """
-    GitHub users search web service.
-    sortlist repository: "followers", "repositories", "joined"
-    """
-    path = base_url + "users?q=%s&sort=%s&order=%s&per_page=%s&page=%s"
+async def users_search(query, sort, order, per_page, page):
+    path = BASE_URL + "users?q=%s&sort=%s&order=%s&per_page=%s&page=%s"
     request = await github_search(path=path % (query, sort, order, per_page, page))
-    if request.status_code != httpx.codes.OK:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {
-            "success": False,
-            "data": None,
-            "error_message": "A problem has occurred on our end"
-        }
-
     return {
         "success": True,
-        "data": request.json()
+        "data": request.json(),
+        "error_message": None
     }
