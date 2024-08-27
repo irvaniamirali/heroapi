30d51ba2e6fcfbc8c520ded0aa338de79a9298e5
AmirAli Irvani <irvaniamirali@proton.me>
2024-08-27 12:51:42 +0330

    Update base64_api.py

--- app/router/api/base64_api.py
+++ app/router/api/base64_api.py
@@ -11,19 +11,28 @@ async def base64encode(text):
     Encode to Base64 format
     """
     string = codecs.encode(text, "utf-8")
-    return base64.b64encode(string)
+    return {
+        "success": True,
+        "data": base64.b64encode(string),
+        "error_message": None
+    }
 
 
-async def b64decode(response, payload, string):
+async def b64decode(response, string):
     """
     Decode from Base64 format
     """
     output = codecs.encode(string, "utf-8")
     try:
-        payload["data"] = base64.b64decode(output)
+        return {
+            "success": True,
+            "data": base64.b64decode(output),
+            "error_message": None
+        }
     except Error:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        payload["success"] = False
-        payload["error_message"] = "This text is not base64"
-
-    return payload
+        return {
+            "success": False,
+            "data": None,
+            "error_message": "This text is not base64"
+        }
