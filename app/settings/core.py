91c4679a1899e32636b8417d0331f358a83f42e2
AmirAli Irvani <irvaniamirali@proton.me>
2024-08-31 23:20:15 +0330

    Update settings.py

---
+++ app/settings/core.py
@@ -0,0 +1,18 @@
+
+description = "- [GitHub](https://github.com/irvaniamirali/HeroAPI)\n- [Telegram](https://t.me/HeroAPI)"
+
+app_config = {
+    "title": "HeroAPI",
+    "description": description,
+    "contact": {
+        "name": "HeroTeam",
+        "url": "https://heroapi.ir",
+        "email": "social.irvaniamirali@gmail.com"
+    },
+    "license_info": {
+        "name": "Released under MIT LICENSE",
+        "identifier": "MIT"
+    },
+    "docs_url": None,
+    "redoc": None
+}
