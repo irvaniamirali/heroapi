d0b3b0b319ec4ca47bb1febd6580882c6df20b26
AmirAli Irvani <irvaniamirali@proton.me>
2024-08-31 23:55:38 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Request, status
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi.openapi.docs import get_swagger_ui_html
 
 from app.routers import routers
 from app.api.paths import paths
@@ -12,18 +11,6 @@ app = FastAPI(**app_config)
 templates = Jinja2Templates(directory="app/templates")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
-@app.get("/docs", include_in_schema=False)
-async def swagger_ui_html():
-    """
-    Return swagger API document
-    """
-    return get_swagger_ui_html(
-        openapi_url="/openapi.json",
-        title="HeroAPI",
-        swagger_favicon_url="static/favicon.png",
-    )
 
 
 @app.exception_handler(status.HTTP_404_NOT_FOUND)
