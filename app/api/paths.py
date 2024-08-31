9b4767f52a0dd51b7d7830fa0539b7bbc5663ff0
AmirAli Irvani <irvaniamirali@proton.me>
2024-08-31 22:48:57 +0330

    Update paths.py

--- app/api/paths.py
+++ app/api/paths.py
@@ -1,5 +1,21 @@
-# Define Paths of API Routers
+# Define the Paths of Routers
 
-paths = [
-    "app.routers.ai.router"
+Paths = [
+    "app.settings.docs.router",  # swagger UI HTML Docs
+    "app.routers.ai.router",
+    "app.routers.base64_api.router",
+    "app.routers.datetime_api.router",
+    "app.routers.duckduckgo.router",
+    "app.routers.faker.router",
+    "app.routers.github.router",
+    "app.routers.lexica_api.router",
+    "app.routers.music_fa.router",
+    "app.routers.news_fa.router",
+    "app.routers.others_api.router",
+    "app.routers.pypi_projects.router",
+    "app.routers.shop.router",
+    "app.routers.translator.router",
+    "app.routers.tron_api.router",
+    "app.routers.v2ray_api.router",
+    "app.routers.wikipedia.router",
 ]
