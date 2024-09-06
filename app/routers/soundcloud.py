e2c7fb631127f8d761f31a4877733e918beaa3f1
AmirAli Irvani <irvaniamirali@proton.me>
2024-09-06 12:11:10 +0330

    Update `soundcloud` Router

--- app/routers/soundcloud.py
+++ app/routers/soundcloud.py
@@ -1,14 +1,23 @@
 from fastapi import APIRouter, Response, status
 
-from app.api.sources.soundcloud import track
+from app.api.sources.soundcloud import track, playlist
 
 router = APIRouter(prefix="/api", tags=["SoundCloud Downloader"])
 
 
 @router.get("/soundcloud/track", status_code=status.HTTP_200_OK)
 @router.post("/soundcloud/track", status_code=status.HTTP_200_OK)
-async def soundcloud(response: Response, url: str) -> dict:
+async def soundcloud_track(response: Response, url: str) -> dict:
     """
-    This API for Download Track at https://soundcloud.com
+    This API for Fetch Track at https://soundcloud.com
     """
     return await track(response, url)
+
+
+@router.get("/soundcloud/playlist", status_code=status.HTTP_200_OK)
+@router.post("/soundcloud/playlist", status_code=status.HTTP_200_OK)
+async def soundcloud_playlist(response: Response, url: str) -> dict:
+    """
+    This API for Fetch Playlist at https://soundcloud.com
+    """
+    return await playlist(response, url)
