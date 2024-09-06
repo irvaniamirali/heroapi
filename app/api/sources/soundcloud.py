fdb0dba23dcc7d6e7840bdac2eaf3ea1408f6b64
AmirAli Irvani <irvaniamirali@proton.me>
2024-09-06 11:55:27 +0330

    Update soundcloud.py

--- app/api/sources/soundcloud.py
+++ app/api/sources/soundcloud.py
@@ -1,12 +1,41 @@
-from sclib import SoundcloudAPI, Track
-from fastapi.responses import  FileResponse
+from fastapi import status
 
-async def soundcloud(link_music):
+from sclib.asyncio import SoundcloudAPI
 
-    api = SoundcloudAPI()
-    track = api.resolve(link_music)
+from httpx import AsyncClient
 
-    assert type(track) is Track
+client = AsyncClient()
 
-    filename = f'./{track.artist} - {track.title}.mp3'
-    return FileResponse(filename)
+api = SoundcloudAPI()
+
+
+async def track(response, url):
+    """
+    SoundCloud Track Downloader
+    """
+    try:
+        track_result = await api.resolve(url)
+    except:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {
+            "success": False,
+            "data": None,
+            "error_message": "A problem has occurred on our end"
+        }
+
+    stream_url = await track_result.get_stream_url()
+    return {
+        "artist": track_result.artist,
+        "title": track_result.title,
+        "artwork_url": track_result.artwork_url,
+        "stream_url": stream_url,
+        "comment_count": track_result.comment_count,
+        "album": track_result.album,
+        "license": track_result.license,
+        "display_date": track_result.display_date,
+        "download_count": track_result.download_count,
+        "likes_count": track_result.likes_count,
+        "public": track_result.public,
+        "policy": track_result.policy,
+        "release_date": track_result.release_date
+    }
