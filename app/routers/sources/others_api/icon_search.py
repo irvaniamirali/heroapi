6f50c788005102625a10894b40f5765f5a50a92d
AmirAli Irvani <irvaniamirali@proton.me>
2024-09-11 04:18:08 +0330

    Update icon_search.py

---
+++ app/routers/sources/others_api/icon_search.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, status
+
+from typing import Optional
+
+from app.api.sources.others_api import icon_search
+
+router = APIRouter(prefix="/api")
+
+
+@router.get("/icon", tags=["Icon search"], status_code=status.HTTP_200_OK)
+@router.post("/icon", tags=["Icon search"], status_code=status.HTTP_200_OK)
+async def icons(query: str, page: Optional[int] = 1) -> dict:
+    """
+    Web Service to search icon from [icon-icons](https://icon-icons.com)
+    """
+    return await icon_search(query, page)
