9390cf83b7621f3041f4a2b03f380297050cc22d
AmirAli Irvani <irvaniamirali@proton.me>
2024-09-11 17:37:46 +0330

    Update convert_html_to_json.py

--- app/routers/sources/others_api/convert_html_to_json.py
+++ app/routers/sources/others_api/convert_html_to_json.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, status
 
+from typing import Optional
+
 from app.api.sources.others_api import convert_html_to_json_api
 
 router = APIRouter(prefix="/api")
@@ -7,8 +9,12 @@ router = APIRouter(prefix="/api")
 
 @router.get("/html2json", tags=["Convert HTML to JSON"], status_code=status.HTTP_200_OK)
 @router.post("/html2json", tags=["Convert HTML to JSON"], status_code=status.HTTP_200_OK)
-async def convert_html_to_json(html: str) -> dict:
+async def convert_html_to_json(
+        html: str,
+        capture_element_values: Optional[bool] = True,
+        capture_element_attributes: Optional[bool] = True
+) -> dict:
     """
     Convert HTML document to json
     """
-    return await convert_html_to_json_api(html)
+    return await convert_html_to_json_api(html, capture_element_values, capture_element_attributes)
