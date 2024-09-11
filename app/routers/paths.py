2283caf8ba6df3d32511f264f64a6af505ebbdad
AmirAli Irvani <irvaniamirali@proton.me>
2024-09-11 04:06:47 +0330

    Update paths.py

--- app/routers/paths.py
+++ app/routers/paths.py
@@ -1,22 +1,35 @@
-# Define the Paths of Routers
+import os
 
-paths = [
-    "app.settings.docs.router",  # Swagger UI HTML Docs
-    "app.routers.sources.ai.router",
-    "app.routers.sources.base64_api.router",
-    "app.routers.sources.datetime_api.router",
-    "app.routers.sources.duckduckgo.router",
-    "app.routers.sources.faker.router",
-    "app.routers.sources.github.router",
-    "app.routers.sources.lexica_api.router",
-    "app.routers.sources.music_fa.router",
-    "app.routers.sources.news_fa.router",
-    "app.routers.sources.others_api.router",
-    "app.routers.sources.pypi_projects.router",
-    "app.routers.sources.shop.router",
-    "app.routers.sources.soundcloud.router",
-    "app.routers.sources.translator.router",
-    "app.routers.sources.tron_api.router",
-    "app.routers.sources.v2ray_api.router",
-    "app.routers.sources.wikipedia.router",
+directory_path, pattern = "app/routers", ".py"
+root_path_of_routers = "app/routers/sources/"
+
+addition_paths = [
+    "app.settings.docs.router"
 ]
+forbidden_files = ["paths.py", "routers.py", "__init__.py"]
+
+
+def list_files_walk(start_path="."):
+    paths = []
+    for root, dirs, files in os.walk(start_path):
+        for file in files:
+            full_path = os.path.join(root, file)
+            if full_path.endswith(pattern) and file not in forbidden_files:
+                paths.append(file)
+    return paths
+
+
+def replace_suffix(paths):
+    routes = []
+    for path in paths:
+        path = root_path_of_routers + path
+        without_suffix_path = path.replace(pattern, ".router")
+        routes.append(without_suffix_path.replace("/", "."))
+    return routes
+
+
+full_paths = list_files_walk(directory_path)
+
+# Path of APIRouters
+paths = sorted(replace_suffix(full_paths))
+paths += addition_paths
