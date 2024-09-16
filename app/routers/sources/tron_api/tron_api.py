bdc944215a2304ab2ed45d2f2dda833e4a3f7637
AmirAli Irvani <irvaniamirali@proton.me>
2024-09-16 14:56:40 +0330

    Update tron_api.py

--- app/routers/sources/tron_api/tron_api.py
+++ app/routers/sources/tron_api/tron_api.py
@@ -1,14 +1,14 @@
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, status
 
-from app.api.sources.tron_api import tron
+from app.api.sources.tron_api import tron as tron_api
 
 router = APIRouter(prefix="/api", tags=["Tron"])
 
 
 @router.get("/tron", status_code=status.HTTP_200_OK)
 @router.post("/tron", status_code=status.HTTP_200_OK)
-async def tron(response: Response) -> dict:
+async def tron() -> dict:
     """
     In this api, by using request and crypto site, we get the current price of Tron currency...
     """
-    return await tron(response)
+    return await tron_api()
