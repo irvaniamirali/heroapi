9c407fe9c6314d223536f9a084f771fa61d3b014
AmirAli Irvani <irvaniamirali@proton.me>
2024-09-16 14:56:21 +0330

    Update tron_api.py

--- app/api/sources/tron_api/tron_api.py
+++ app/api/sources/tron_api/tron_api.py
@@ -7,27 +7,15 @@ from bs4 import BeautifulSoup
 client = AsyncClient()
 
 
-async def tron(response):
+async def tron():
     """
     In this sources, by using request and crypto site, we get the current price of Tron currency...
     """
-    request = await client.request("POST", url="https://arzdigital.com/coins/tron/")
-    if request.status_code != codes.OK:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {
-            "success": False,
-            "data": None,
-            "error_message": "A problem has occurred on our end"
-        }
-
-    soup = BeautifulSoup(request.content, "html.parser")
+    response = await client.request("POST", url="https://arzdigital.com/coins/tron/")
+    soup = BeautifulSoup(response.content, "html.parser")
     price = soup.find(
         "div", {
             "class": "arz-coin-page-data__coin-toman-price"
         }
     )
-    return {
-        "success": True,
-        "data": price.text.strip(),
-        "error_message": None
-    }
+    return {"output": price.text.strip()}
