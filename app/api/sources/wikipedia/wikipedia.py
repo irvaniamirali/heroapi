8ddaa96ddca9b59f023924f591e0beeb9417afdb
AmirAli Irvani <irvaniamirali@proton.me>
2024-09-16 15:05:32 +0330

    Update wikipedia.py

--- app/api/sources/wikipedia/wikipedia.py
+++ app/api/sources/wikipedia/wikipedia.py
@@ -1,26 +1,12 @@
-from fastapi import status
-
-from httpx import AsyncClient, codes
+from httpx import AsyncClient
 
 client = AsyncClient()
 
 
-async def search(response, query, lang, format):
+async def search(query, lang, _format):
     """
     Do a Wikipedia search for `query`.
     """
     base_url = "https://%s.wikipedia.org/w/api.php?action=parse&page=%s&format=%s"
-    request = await client.request(method="GET", url=base_url % (lang, query, format))
-    if request.status_code != codes.OK:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        return {
-            "success": False,
-            "data": None,
-            "error_message": "A problem has occurred on our end"
-        }
-
-    return {
-        "success": True,
-        "data": request.json(),
-        "error_message": False
-    }
+    response = await client.request(method="GET", url=base_url % (lang, query, _format))
+    return response.json()
