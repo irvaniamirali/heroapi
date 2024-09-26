d460086625c80ff3419e806bc3e1812b76629050
AmirAli Irvani <irvaniamirali@proton.me>
2024-09-26 14:21:18 +0330

    Update shop.py

--- app/routers/sources/shop/shop.py
+++ app/routers/sources/shop/shop.py
@@ -8,7 +8,6 @@ router = APIRouter(prefix="/api", tags=["Divar Products search"])
 
 
 @router.get("/divar", status_code=status.HTTP_200_OK)
-@router.post("/divar", status_code=status.HTTP_200_OK)
 async def divar_products_search(query: str, city: Optional[str] = "tehran") -> list:
     """
     Web search service in [Divar](https://divar.ir).
