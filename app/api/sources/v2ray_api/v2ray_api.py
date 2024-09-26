8e81082aa7886c9b5f6faf5d1420d69576e295b2
AmirAli Irvani <irvaniamirali@proton.me>
2024-09-26 14:29:18 +0330

    Update v2ray_api.py

--- app/api/sources/v2ray_api/v2ray_api.py
+++ app/api/sources/v2ray_api/v2ray_api.py
@@ -1,18 +1,25 @@
-from httpx import AsyncClient, codes
-
 from random import randint
+from httpx import AsyncClient
+
+import user_agent
 
 client = AsyncClient()
 
 GITHUB_REPO = "https://github.com/barry-far/V2ray-Configs"
 RAW_URL = "https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/All_Configs_Sub.txt"
 
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:47.0) Gecko/20100101 Firefox/47.0"
+}
+
 
 async def v2ray(count):
     """
     Get free v2ray configs (any types)
     """
-    response = await client.request(method="GET", url=RAW_URL)
+    HEADERS["User-Agent"] = user_agent.generate_user_agent()
+    response = await client.request(method="GET", url=RAW_URL, headers=HEADERS)
+    response.raise_for_status()
     configs = response.text.splitlines()
 
     for config in configs:
