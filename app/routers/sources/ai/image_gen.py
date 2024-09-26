d5e8fc298d1f98b7bfbfe21d4b39ee2b013efcdf
AmirAli Irvani <irvaniamirali@proton.me>
2024-09-26 14:32:39 +0330

    Rename `lexica_api.py` to `image_gen.py`

--- app/routers/sources/ai/image_gen.py
+++ app/routers/sources/ai/image_gen.py
@@ -5,7 +5,7 @@ from app.api.sources.ai import lexica_api
 router = APIRouter(prefix="/api", tags=["AI", "Image Generator"])
 
 
-@router.get("/lexica", status_code=status.HTTP_200_OK)
+@router.get("/lexica", summary="Image Generator by AI", status_code=status.HTTP_200_OK)
 async def lexica(query: str) -> dict:
     """
     AI Image Generator. [lexica](https://lexica.art)
