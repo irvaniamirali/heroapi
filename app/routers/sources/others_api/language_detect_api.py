e64de49c1b49d746862f960fb2549f0f43fee9e5
AmirAli Irvani <irvaniamirali@proton.me>
2024-09-26 14:13:39 +0330

    Update language_detect.py

--- app/routers/sources/others_api/language_detect_api.py
+++ app/routers/sources/others_api/language_detect_api.py
@@ -2,11 +2,10 @@ from fastapi import APIRouter, Response, status
 
 from app.api.sources.others_api import language_detect_api
 
-router = APIRouter(prefix="/api")
+router = APIRouter(prefix="/api", tags=["Language detect"])
 
 
-@router.get("/lang", tags=["Language detect"], status_code=status.HTTP_200_OK)
-@router.post("/lang", tags=["Language detect"], status_code=status.HTTP_200_OK)
+@router.get("/lang", status_code=status.HTTP_200_OK)
 async def language_detect(response: Response, text: str) -> dict:
     """
     Identifying the language of texts
