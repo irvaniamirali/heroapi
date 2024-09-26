0a1b2770793bd5f0a8722153e4da8546eb9e3fdd
AmirAli Irvani <irvaniamirali@proton.me>
2024-09-26 14:12:57 +0330

    Update icon_search.py

--- app/routers/sources/others_api/icon_search.py
+++ app/routers/sources/others_api/icon_search.py
@@ -8,7 +8,6 @@ router = APIRouter(prefix="/api")
 
 
 @router.get("/icon", tags=["Icon search"], status_code=status.HTTP_200_OK)
-@router.post("/icon", tags=["Icon search"], status_code=status.HTTP_200_OK)
 async def icons(query: str, page: Optional[int] = 1) -> list:
     """
     Web Service to search icon from [icon-icons](https://icon-icons.com)
