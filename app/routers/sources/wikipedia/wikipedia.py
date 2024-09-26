ead4eb7b5b2cd9d8b0af19ecae7f0a9cbd9c78d4
AmirAli Irvani <irvaniamirali@proton.me>
2024-09-26 14:29:55 +0330

    Update wikipedia.py

--- app/routers/sources/wikipedia/wikipedia.py
+++ app/routers/sources/wikipedia/wikipedia.py
@@ -8,7 +8,6 @@ router = APIRouter(prefix="/api", tags=["WikiPedia article search"])
 
 
 @router.get("/wikipedia/search", status_code=status.HTTP_200_OK)
-@router.post("/wikipedia/search", status_code=status.HTTP_200_OK)
 async def wikipedia(query: str, lang: Optional[str] = "en", _format: Optional[str] = None) -> dict:
     """
     Do a Wikipedia search for `query`.
